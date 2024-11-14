de7afbc19d3866a3370ac5c4a6ab41bace242082
Ankush Joshi <joshiankush6@gmail.com>
2024-11-14 15:23:10 -0800

    pause menu animation

--- TDGame/Assets/PauseMenu.cs
+++ TDGame/Assets/PauseMenu.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using System.Threading.Tasks;
 
 public class PauseMenu : MonoBehaviour
 {
 
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] RectTransform pausePanelRect;
+    [SerializeField] float topPos, midPos;
+    [SerializeField] float tweenDuration;
+    [SerializeField] CanvasGroup canvasGroup;
 
     public void Pause()
     {
+        pausePanelIn();
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
     }
@@ -20,10 +27,25 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1;
 
     }
-
-    public void Resume()
+    
+    public async void Resume()
     {
+        await pausePanelOut();
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
+    }
+
+
+    void pausePanelIn()
+    {
+        canvasGroup.DOFade(1, tweenDuration).SetUpdate(true);
+        pausePanelRect.DOAnchorPosY(midPos, tweenDuration).SetUpdate(true);
+    }
+
+    async Task pausePanelOut()
+    {
+        canvasGroup.DOFade(0, tweenDuration).SetUpdate(true);
+        await pausePanelRect.DOAnchorPosY(topPos, tweenDuration).SetUpdate(true).AsyncWaitForCompletion();
+
     }
 }
